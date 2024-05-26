d351491d9e3c89d7a64a56394d48c900713f8b13
Tirth Patel <pateltirth2202@gmail.com>
2024-05-26 23:59:53 +0530

    Functionality of addtocart button is done

--- addtocart/src/App.js
+++ addtocart/src/App.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Shop from './components/Shop'
+import './App.css'
 const App = () => {
+  const[cart,setCart] = useState([]);
+  const[warning,setWarning] = useState(false);
+
+
+  const handleClick = (item) =>{
+    let isPresent = false;
+    cart.forEach((product) =>{
+      if(item.id === product.id)
+        isPresent = true;
+    })
+    if(isPresent)
+    {
+      setWarning(true);
+      setTimeout(() =>{
+        setWarning(false)
+      },2000);
+      return
+    }
+    setCart([...cart,item]);
+  }
   return (
     <div>
-      <Navbar/>
-      <Shop/>
+      <Navbar size={cart.length}/>
+      <Shop handleClick ={handleClick}/>
+      {warning && <div className='warning'>
+        Item is Already Present in Cart</div>}
     </div>
   )
 }
